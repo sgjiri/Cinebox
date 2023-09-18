8f7d34e4e947230efa3b380d7f0bdc9d780c1497
sg.jiri <sg.jiri@yahoo.fr>
2023-09-18 11:22:07 +0200

    tableauGenres

--- movie.js
+++ movie.js
@@ -1,4 +1,65 @@
-import {createTitle, createLien, createResume} from "./components/movieCard";
+import { createFirstTitle, createSecondeTitle, createLien, createParagraph, createPoster, createYeur, createGenre } from "./components/movieCard";
+
+
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NDJkMzQ2YjJmYzU4ZDY5ODFkYzA2OGU3NzQxYjRlZiIsInN1YiI6IjY0ZmYxM2E5NmEyMjI3MDEzNzJjYjlkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SR0SRMd26YudmAbU-CGdCbydluRdJC8yHgDZ043oRqE'
+  }
+};
+
+
+
+fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
+  .then(response => response.json())
+  .then(response => {
+    let app = document.getElementById('app');
+    for (let movie of response.results) {
+      let newDiv = document.createElement('div');
+      newDiv.className = 'oneFilm'
+      app.appendChild(newDiv);
+      newDiv.appendChild(createSecondeTitle(movie.title));
+      newDiv.appendChild(createParagraph(movie.overview));
+      newDiv.appendChild(createLien(movie.id));
+
+
+
+
+    }
+    let lienDetailFilm = document.getElementsByClassName('lienDetailFilm');
+    let oneFilm = document.getElementsByClassName('oneFilm');
+    for (let i = 0; i < lienDetailFilm.length; i++) {
+
+      lienDetailFilm[i].addEventListener('click', function () {
+
+        for (let j = 0; j < oneFilm.length; j++) {
+          oneFilm[j].classList.add('invisible');
+        }
+        const hrefValue = lienDetailFilm[i].getAttribute('href');
+
+        fetch(`https://api.themoviedb.org/3/movie/${hrefValue}?language=fr`, options)
+          .then(response => response.json())
+          .then(response => {
+            let newDivDetil = document.createElement('div');
+            app.appendChild(newDivDetil);
+            console.log(response.runtime);
+            newDivDetil.appendChild(createPoster(response.poster_path));
+            newDivDetil.appendChild(createFirstTitle(response.title));
+            newDivDetil.appendChild(createYeur(response.release_date));
+            newDivDetil.appendChild(createGenre(response.genres));
+          })
+          .catch(err => console.error(err));
+
+      })
+    }
+  })
+  .catch(err => console.error(err));
+
+
+
+
 
 
 
@@ -23,14 +84,6 @@ import {createTitle, createLien, createResume} from "./components/movieCard";
 
 // console.log(player1.presentation());
 
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NDJkMzQ2YjJmYzU4ZDY5ODFkYzA2OGU3NzQxYjRlZiIsInN1YiI6IjY0ZmYxM2E5NmEyMjI3MDEzNzJjYjlkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SR0SRMd26YudmAbU-CGdCbydluRdJC8yHgDZ043oRqE'
-    }
-  };
-  
 //   fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
 //     .then(response => response.json())
 //     .then(response => {
@@ -43,18 +96,3 @@ const options = {
 //         }
 //     })
 //     .catch(err => console.error(err));
-
-fetch('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
-.then(response => response.json())
-.then(response => {
-    let app = document.getElementById('app');
-    for(let movie of response.results){
-        let newDiv = document.createElement('div');
-        app.appendChild(newDiv);
-        newDiv.appendChild(createTitle(movie.title));
-        newDiv.appendChild(createResume(movie.overview));
-        newDiv.appendChild(createLien(movie.id));
-        
-    }
-})
-.catch(err => console.error(err));
